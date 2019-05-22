ee6b5e4e2fdd370ae1d0bd7801fdb4c62a7af4f2
pedro <pedro.hgm30@gmail.com>
2019-05-22 12:13:12 -0300

    tinymce bug fixed

--- app/assets/javascripts/editor.js
+++ app/assets/javascripts/editor.js
@@ -3,9 +3,9 @@ $(document).on('ready page:load', function() {
 		tinyMCE.init({
 			selector : 'textarea.tinymce',
 			toolbar  : [
-				'styleselect | bold italic | undo redo image media | link | charmap | numlist bullist | codesample | fullscreen preview'
+				'styleselect | bold italic | undo redo image media | link | charmap | numlist bullist | codesample | fullscreen preview | nonbreaking'
 			],
-			plugins  : '  - image, link, charmap, lists, advlist, codesample, fullscreen, media, preview'
+			plugins  : '  - image, link, charmap, lists, advlist, codesample, fullscreen, media, preview, nonbreaking'
 		});
 	}
 	else {
