aa7e51c9554c4940cb5ad84b19d26211dfb760c5
pedro <pedro.hgm30@gmail.com>
2019-05-24 14:45:04 -0300

    commenting some models to make them easier to understand

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,13 @@
 class Comment < ApplicationRecord
+  
+  # a comment must be associeted with a post
+  # but not necessarily with a user or an author
+  # since they can delete their accounts 
+  # but the comment will stay there
+
   belongs_to :post
   belongs_to :user, optional: true
   belongs_to :author, optional: true
   validates :body, presence: true
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,15 @@
 class Post < ApplicationRecord
-  validates :title, presence: true
-	validates :body, presence: true
+  validates :title, :body, presence: true
   belongs_to :author
+
+  # without this dependency relation
+  # wouldn't be possible to delete a post with comments
   has_many :comments, dependent: :delete_all
   
+  # this syntax is specific from the friendly_id gem
+  # It's using the post title as a slug
+  # the slug is used to identify the post instead of the id
+  # this leads to a better UX
   extend FriendlyId
   friendly_id :title, use: :slugged
 
@@ -11,6 +17,9 @@ class Post < ApplicationRecord
     title_changed?
   end
 
+  # this is part of the search logic
+  # it receives a query as an argument
+  # and search for that on the database
   def self.search(query)
   	where("title like ? OR body like ?", "%#{query}%", "%#{query}%")
   end
