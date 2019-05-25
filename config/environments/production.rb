6d3e2f1de8dd48247294e5dd7faf167e3d5766a4
pedro <pedro.hgm30@gmail.com>
2019-05-25 16:28:23 -0300

    minor fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,5 +92,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { host: 'https://theoneblog.herokuapp.com/ ' }
-  config.assets.paths << Rails.root.join("app", "assets", "fonts")
+  config.assets.paths << Rails.root.join("app", "assets", "fonts", "roboto")
 end
